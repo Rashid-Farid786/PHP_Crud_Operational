862e9e1bd527c6f010a9dd17eef3790aa1b59d60
Rashid-Farid <rashidfarid@gmail.com>
2023-05-12 17:11:28 +0500

    add compoaer.loack

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rashidfarid/php_crud_operational',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2769a96264f18548447e42dbaa2ce83f04db9727',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rashidfarid/php_crud_operational' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2769a96264f18548447e42dbaa2ce83f04db9727',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

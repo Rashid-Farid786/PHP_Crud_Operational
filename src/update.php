257db46b1e2c4cf56a6f018d86004b77500227fb
Rashid Farid <rashidfarid40@gmail.com>
2023-11-01 18:24:05 +0500

    r

--- src/update.php
+++ src/update.php
@@ -5,15 +5,21 @@ session_start();
 // print_r($_SESSION);die;
 use MyApp\Connection;
 $con=mysqli_connect($_SESSION['crud']['h'],$_SESSION['crud']['u'],$_SESSION['crud']['p'],$_SESSION['crud']['d']);
-$sql="SHOW FIELDS FROM {$_SESSION['crud']['t']}";
+$sql="SELECT * FROM {$_SESSION['crud']['t']}";
 $result=mysqli_query($con,$sql);
 if(mysqli_num_rows($result)>0){
-    $data=mysqli_fetch_assoc($result);
-        $keys=implode('=>',$data);
-        echo "<pre>";
-    print_r($keys);
-
+    $string="";
+    foreach($data=mysqli_fetch_assoc($result) as $key=>$value){
+        $string.=$key."=".$value." , ";
+    }
+    $sql1="UPDATE {$_SESSION['crud']['t']} SET trim($string,',')";
+    if(mysqli_query($con,$sql1)){
+        echo 1;
+    }else{
+        echo 2;
+    }
+    
 }else{
-
+    echo 3;
 }
 ?>
